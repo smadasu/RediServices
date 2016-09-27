70fe10cc7cbed8181138cd4f70ca13a521b419e1
Sreenath Madasu <Sreenath.Madasu@gmail.com>
2016-09-26 20:58:00 -0400

    Added exception handling

--- src/main/java/org/ms/service/JedisServices.java
+++ src/main/java/org/ms/service/JedisServices.java
@@ -4,15 +4,19 @@ import java.util.Optional;
 
 import javax.annotation.PreDestroy;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 @Component("jedisServices")
 public class JedisServices {
+	
+	private static Logger log = Logger.getLogger(JedisServices.class);
 	
 	@Value("${redis-host}")
     private String redisHost;	
@@ -34,9 +38,17 @@ public class JedisServices {
 	}
 	
 	public Optional<String> getValue(String key) {
+		String returnValue = null;
 		try (Jedis jedis = getPool().getResource()) {
-			return Optional.ofNullable(jedis.get(key));
+			returnValue = jedis.get(key);
+		} catch(JedisConnectionException e) {
+			log.error("Error while getting connection", e);
+			returnValue = e.getCause().getLocalizedMessage();
+		} catch(Exception e) {
+			log.error("Error while getValue", e);
+			returnValue = e.getCause().getLocalizedMessage();
 		}
+		return Optional.ofNullable(returnValue);
 	}
 	
 	public String setValue(String key, String value) {
