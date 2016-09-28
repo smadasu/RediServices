cff7f95f53aea63d690cdd594ae5367daa84a2bd
Sreenath Madasu <Sreenath.Madasu@gmail.com>
2016-09-27 21:18:16 -0400

    cleaned up import

--- src/main/java/org/ms/controller/MainController.java
+++ src/main/java/org/ms/controller/MainController.java
@@ -7,12 +7,10 @@ import java.util.Set;
 
 import org.ms.service.JedisServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
